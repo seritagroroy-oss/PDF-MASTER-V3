8caa4df8481a76d05f0635331188c58b1e50c87c
seritagroroy-oss <seritagroroy-oss@users.noreply.github.com>
2026-04-02 13:57:00 +0000

    fix(pdf): fix pwa cache by using vite worker url import

--- src/pdfjs-setup.ts
+++ src/pdfjs-setup.ts
@@ -1,6 +1,7 @@
 import * as pdfjs from 'pdfjs-dist';
 
-// Use unpkg CDN to always match the exact version of pdfjs-dist installed and avoid Vite ESM fetching issues
-pdfjs.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
+import pdfWorker from 'pdfjs-dist/build/pdf.worker.min.mjs?url';
+
+pdfjs.GlobalWorkerOptions.workerSrc = pdfWorker;
 
 export { pdfjs };
